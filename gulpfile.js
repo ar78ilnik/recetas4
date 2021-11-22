5c317a3f422caec78ec4ecdd762dd22a612f551b
ar78ilnk <arakchiev.ilya@yandex.ru>
2021-11-22 21:23:41 +0300

    New mobile font

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ function watch() {
     gulp.watch('app/scss/*.scss', style);
     gulp.watch('app/js/**/*.js', js);
     gulp.watch('app/fonts/*.{woff, woff2}', copy);
-    gulp.watch('app/img/**/*.jpg', toWebp);
+    gulp.watch('app/img/**/*.jpg', images);
     gulp.watch('app/img/**/*.svg', sprite);
 };
 
