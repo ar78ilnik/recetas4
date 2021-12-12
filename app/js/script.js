d82fc89304d1449b2572dc36e06621aa9dc172a0
ar78ilnik <arakchiev.ilya@yandex.ru>
2021-12-12 18:50:38 +0300

    Icon svg

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,9 @@
     const mainBlock = document.querySelector('.header__menu'),
         svgImg = mainBlock.querySelector('.header__svg'),
-        menuMain = document.querySelector('.menu_main'),
+        menuMain = mainBlock.querySelector('.menu_main'),
         bodyBack = document.querySelector('.body__back'),
-        menuClose = document.querySelector('.menu__close');
+        menuClose = mainBlock.querySelector('.menu__close');
+
     mainBlock.addEventListener('click', function() {
         let expanded = mainBlock.getAttribute('aria-expanded') === 'true' || false;
         mainBlock.setAttribute('aria-expanded', !expanded);
@@ -14,7 +15,6 @@
     menuClose.addEventListener('click', function() {
         let expanded = mainBlock.getAttribute('aria-expanded') === 'true' || false;
         menuMain.classList.toggle('menu_opened');
-        console.log('1');
         bodyBack.classList.toggle('body__back_opened');
         mainBlock.setAttribute('aria-expanded', !expanded);
         svgImg.classList.toggle('header__svg_rotated');
